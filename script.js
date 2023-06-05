52cf5e11b156d8a4ebe8bb7ddcd0bac8259923c5
phiroun <man.phiroun@gmail.com>
2023-06-05 15:16:14 -0700

    update script file

---
+++ script.js
@@ -0,0 +1,28 @@
+function Car(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+}
+
+var myCar = new Car("Toyota", "Camry", 2022);
+console.log(myCar.getDescription());
+
+Car.prototype.getDescription = function () {
+  return "This is a " + this.year + " " + this.make + " " + this.model + ".";
+};
+
+function ElectricCar(make, model, year, range) {
+  Car.call(this, make, model, year);
+  this.range = range;
+}
+
+ElectricCar.prototype = Object.create(Car.prototype);
+ElectricCar.prototype.constructor = ElectricCar;
+
+ElectricCar.prototype.getDescription = function () {
+  var carDescription = Car.prototype.getDescription.call(this);
+  return carDescription + " It has a range of " + this.range + " miles.";
+};
+
+var myElectricCar = new ElectricCar("Tesla", "Model S", 2019, 300);
+console.log(myElectricCar.getDescription());
